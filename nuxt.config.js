e366e0e2e1478f4cd01e436c28ca7c7ce7cdcae1
Daniele Tabanella <daniele.tabanella@gmail.com>
2021-09-22 01:12:34 +0200

    fix(axios): base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,9 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: '/'
+  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
